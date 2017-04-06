d312b249d3e8b54a386d3ea26c7db11b1e3c0e57
janbikz <bikram.dahal@cqumail.com>
2017-04-06 17:18:27 +1000

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<h1>Hello World</h1>'; ?> 
+ <?php 
+  
+$txt1 = "Hello World";
+$txt2 = "App by Bikram Dahal";
+$x = 5;
+$y = 4;
+
+echo "<h2>$txt1</h2>";
+echo "Study PHP at $txt2<br>";
+echo $x + $y;
+
+  ?> 
  </body>
 </html>
